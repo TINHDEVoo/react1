1c7262144b53b8d61c667e824403ba61b8559233
TinhDev <tinhdev@gmail.com>
2024-08-18 11:53:51 +0700

    video 47

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+const Header = () => {
+    return (
+        <>
+            <ul>
+                <li><a class="active" href="/">Home</a></li>
+                <li><a href="/Users">Users</a></li>
+                <li><a href="/product">Product</a></li>
+            </ul>
+
+        </>
+    )
+}
+
+export default Header
