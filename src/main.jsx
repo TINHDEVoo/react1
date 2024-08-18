6d5dffbdcfa8e9613be0f7a1bf3538272573f2ad
TinhDev <tinhdev@gmail.com>
2024-08-18 20:42:43 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,15 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UsersPage from './pages/users.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import TodoApp from './components/todo/TodoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -27,8 +29,8 @@ const router = createBrowserRouter([
         element: <UsersPage />
       },
       {
-        path: "/product",
-        element: <ProductPage />
+        path: "/book",
+        element: <BookPage />
       }
     ]
   },
