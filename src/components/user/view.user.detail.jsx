54ae22357b935457b3e984008acc619160e4e6e6
TinhDev <tinhdev@gmail.com>
2024-08-21 21:36:57 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserModal = (props) => {
     const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
@@ -21,6 +21,25 @@ const ViewUserModal = (props) => {
                     <p>Email: {dataDetail.email}</p>
                     <br />
                     <p>Phone number: {dataDetail.phone}</p>
+                    <br />
+                    <p>Avatar:</p>
+                    <div>
+                        <img height={150} width={150}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar} `} alt="" />
+                    </div>
+                    <div>
+                        <label htmlFor="btnUpload"
+                            style={{
+                                backgroundColor: "green",
+                                padding: "10px 20px",
+                                borderRadius: "50px",
+                                color: "#fff"
+                            }}>
+                            Upload Avatar
+                        </label>
+                        <input type="file" id="btnUpload" hidden />
+                    </div>
+                    {/* <Button type="primary">Upload Avatar</Button> */}
                 </>
                     :
                     <>
