4456f57889c14c76af2185e04ac7afb4a5eafa16
TinhDev <tinhdev@gmail.com>
2024-08-21 22:01:59 +0700

    video 83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,8 +1,25 @@
 import { Button, Drawer } from "antd";
+import { useState } from "react";
 
 const ViewUserModal = (props) => {
     const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+    const handleOnChangeFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            setPreview(null)
+            return
+        }
 
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0]
+        if (file) {
+            setSelectedFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
+    console.log("Check file: ", preview)
     return (
         <Drawer
             title="View a User"
@@ -37,9 +54,17 @@ const ViewUserModal = (props) => {
                             }}>
                             Upload Avatar
                         </label>
-                        <input type="file" id="btnUpload" hidden />
+                        <input type="file" id="btnUpload" hidden
+                            // onChange={handleOnChangeFile}
+                            onChange={(event) => handleOnChangeFile(event)}
+                        />
                     </div>
-                    {/* <Button type="primary">Upload Avatar</Button> */}
+                    {preview &&
+                        <div>
+                            <img height={150} width={150}
+                                src={preview} alt="" />
+                        </div>
+                    }
                 </>
                     :
                     <>
