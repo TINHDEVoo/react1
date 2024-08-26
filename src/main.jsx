272c90ab9ba1147d977c46b5eda6bd26e10068d6
TinhDev <tinhdev@gmail.com>
2024-08-26 17:12:21 +0700

    chuong 11 and video 112

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
+import 'nprogress/nprogress.css';
 
 import {
   createBrowserRouter,
--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -1,7 +1,10 @@
+import BookTable from "../components/book/book.table"
 
 const BookPage = () => {
     return (
-        <div>Book Page</div>
+        <div style={{ padding: "20px" }}>
+            <BookTable />
+        </div>
     )
 }
 
